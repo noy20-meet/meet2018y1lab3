5ecf7dc38a14710dc509aa770b376b2f7a753f96
noy20-meet <noy20@meet.mit.edu>
2018-07-31 03:25:58 -0400

    Stopping here

---
+++ HelloWorld.py
@@ -0,0 +1,6 @@
+language="English"
+translation="hello world"
+print("the translation of hello world in "+(language)+' is '+(translation)+" using the varibles you've created.remember what "+ " does!")
+language="italian"
+translation="Ciao Mondo"
+print("the translation of hello world in "+(language)+' is '+(translation)+" using the varibles you've created.remember what "+ " does!")
---
+++ spicialCharacters.py
@@ -0,0 +1,2 @@
+print("\\Day1\\")
+print("Alex asks, \"what are you learning today?\" \n the students reply, \'we are learning how to print!\' \n we can type print (\"\\\day1\\\") to print out \\day1\\" )
